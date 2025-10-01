2752266f54d6f10a1d54aa5d030a30a37b809ef3
rozmarin <romaostafeychuk@gmail.com>
2025-10-01 16:59:41 +0300

    add 3 tests

--- LIRS/LIRS_tests.cc
+++ LIRS/LIRS_tests.cc
@@ -25,9 +25,8 @@ int run_cache(size_t ch_size, int N_pushes, std::vector<int> pages) {
 
 TEST(LIRSTest, ElevenHitsCountedByMe) {
     size_t ch_size = 5;
-    int N = 15;
     std::vector<int> pages = {1, 2, 3, 4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2, 1};
-    EXPECT_EQ(run_cache(ch_size, N, pages), 4);
+    EXPECT_EQ(run_cache(ch_size, pages.size(), pages), 4);
 }
 
 TEST(LIRSTest, RepeatedAccess) {
@@ -42,3 +41,31 @@ TEST(LIRSTest, EdgeCaseSize2) {
     int hits = run_cache(ch_size, pages.size(), pages);
     EXPECT_EQ(hits, 1);
 }
+
+TEST(LIRSTest, LargeCache) {
+    size_t ch_size = 10000000;
+    int res = ch_size;
+    std::vector<int> pages;
+    for(int i = 0; i < ch_size; i++) {
+        pages.push_back(i);
+    }
+    for(int i = 0; i < ch_size; i++) {
+        pages.push_back(i);
+    }
+    EXPECT_EQ(run_cache(ch_size, pages.size(), pages), res);
+}
+
+TEST(LIRSTest, HIRNonResidentRecovery) {
+    size_t ch_size = 3;
+    std::vector<int> pages = {1, 2, 3, 4, 5, 4};
+    int hits = run_cache(ch_size, pages.size(), pages);
+    EXPECT_EQ(hits, 0);  
+}
+
+TEST(LIRSTest, TwoLirResidents) {
+    size_t ch_size = 3;
+    std::vector<int> pages = {1, 2, 3, 4, 1, 2, 3, 4};
+    EXPECT_EQ(run_cache(ch_size, pages.size(), pages), 2);
+}
+
+
